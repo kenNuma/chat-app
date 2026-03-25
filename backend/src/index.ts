680376187dbcabd6d86cbea3876bc62170bc6a1f
Kamura <kamura@example.com>
2026-03-25 09:41:31 +0900

    front/backのつなぎ込み

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,30 @@
-import express from "express";
+import express, {Request, Response} from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import type { ChatRequest, ChatResponse } from '../../shared/types';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (_req, res) => {
-    res.send("Backend is running!");
+app.get("/api/message", (req: Request, res: Response) => {
+    res.json({
+        message: "バックエンドからのレスポンス！"
+    });
 });
+
+app.post('/api/chat', (req: Request<{},{},ChatRequest>, res: Response<ChatResponse>) => {
+    const { text } = req.body;
+
+    res.json({
+        reply: `「${text}」を受け取ったよ`,
+    })
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ shared/types.ts
@@ -0,0 +1,7 @@
+export type ChatRequest = {
+    text: string;
+};
+
+export type ChatResponse = {
+    reply: string;
+};
